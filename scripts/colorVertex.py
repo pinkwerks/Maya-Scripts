e2f7d64b50644dc60be1f8b07b9daa49ecb01994
Pink Bobsledder <pinkwerks@gmail.com>
2019-06-11 11:08:07 -0700

    Better stand alone example

--- scripts/colorVertex.py
+++ scripts/colorVertex.py
@@ -1,4 +1,22 @@
-def colorVertex():
-	"""color verticies of a mesh example"""
-	shp = PyNode('pSphereShape1')
-	[shp.vtx[v].setColor((v%10,1,0,1)) for v in shp.vtx.indicesIter()]
+import random
+
+def randomColor():
+    r = random.randrange(256) / 255.0
+    g = random.randrange(256) / 255.0
+    b = random.randrange(256) / 255.0
+    a = random.randrange(256) / 255.0
+    return (r,g,b,a)
+
+def colorVertex(obj):
+    """color verticies of a mesh example"""
+    shape = selected()[0].getShapes()[0]
+    print("shp=", shape)
+    
+    [shape.vtx[v].setColor(randomColor()) for v in shape.vtx.indicesIter()]
+
+def main():
+    objs=selected()
+    [colorVertex(x) for x in objs]
+
+if __name__ == '__main__':
+    main()
